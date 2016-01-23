da5e27362719e6a88f74d67fa3212a07e734ab8c
Faris Ansari <netchamp.faris@gmail.com>
2016-01-24 01:27:19 +0530

    added timetable functionality

--- app/scripts/config/Urls.js
+++ app/scripts/config/Urls.js
@@ -4,4 +4,5 @@ angular.module('HazriSV')
 
   // development
   .constant('FirebaseUrl', 'https://hazri.firebaseio.com')
-  .constant('apiUrl','http://bvcoeportal.orgfree.com/api/student_att_calc.php/');
+  .constant('apiUrl','http://bvcoeportal.orgfree.com/api/student_att_calc.php/')
+  .constant('timetableUrl','https://hazrisv.firebaseio.com/timetables');
